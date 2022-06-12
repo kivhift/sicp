39aa8ace6182d309c351f690fe7eb5a3dade89d1
Joshua Hughes <kivhift@gmail.com>
2022-06-12 16:14:13 -0400

    Add ? to test-OK since it's a predicate

--- ch1/t-ex45.scm
+++ ch1/t-ex45.scm
@@ -7,14 +7,14 @@
 ; The prior experiments imply that the needed average-damp count is the floor
 ; of the binary logarithm of the root count n.
 
-(define (test-OK x n) (close-enough? (expt x (/ 1 n)) (nth-root-of x n)))
+(define (test-OK? x n) (close-enough? (expt x (/ 1 n)) (nth-root-of x n)))
 (define (tests-OK?)
   (and
-    (test-OK 2 2)
-    (test-OK 2 3)
-    (test-OK 2 4)
-    (test-OK 42 13)
-    (test-OK 1e10 63)
-    (test-OK 1e10 257)
-    (test-OK 42 42)
+    (test-OK? 2 2)
+    (test-OK? 2 3)
+    (test-OK? 2 4)
+    (test-OK? 42 13)
+    (test-OK? 1e10 63)
+    (test-OK? 1e10 257)
+    (test-OK? 42 42)
 ))
